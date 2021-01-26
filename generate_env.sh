cfdec7773fd863b9566ffd07cbbdf4d21b4e248b
Kacper Żuk <kacper.b.zuk@gmail.com>
2021-01-26 18:29:46 +0100

    Random updates

--- generate_env.sh
+++ generate_env.sh
@@ -12,7 +12,6 @@ echo -n > .env
 ask DOMAIN "Domain name: "
 ask LE_EMAIL "Email (for LetsEncrypt notifications): "
 ask HTTPS_LISTEN_ADDR "HTTPS listen addr (0.0.0.0 for all interfaces): "
-ask CF_EMAIL "Email of Cloudflare account: "
-ask CF_API_KEY "Cloudflare API key (will be stored in .env file): "
+ask CF_API_TOKEN "Cloudflare API key (will be stored in .env file): "
 ask CF_DOMAIN "Cloudflare domain (Zone) to update record in: "
 ask CF_RECORD "CloudFlare record (subdomain) to update: "
